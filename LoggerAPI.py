771504cbcfe8d64e86b2fd25754ea2de5bedf768
jwrolson <jacob.wrolson@gmail.com>
2017-01-23 11:58:13 -0800

    Create LoggerAPI.py

---
+++ LoggerAPI.py
@@ -0,0 +1,63 @@
+import os
+import json
+import logging
+import logging.handlers
+import configparser
+import falcon
+import signal
+import requests
+
+class RequireJSON(object):
+
+    def process_request(self, req, resp):
+        if not req.client_accepts_json:
+            raise falcon.HTTPNotAcceptable(
+                'This API only supports responses encoded as JSON.',
+                href='http://docs.examples.com/api/json')
+
+        if req.method in ('POST', 'PUT'):
+            if 'application/json' not in req.content_type:
+                raise falcon.HTTPUnsupportedMediaType(
+                    'This API only supports requests encoded as JSON.',
+                    href='http://docs.examples.com/api/json')
+
+class JSONTranslator(object):
+
+    def process_request(self, req, resp):
+        # req.stream corresponds to the WSGI wsgi.input environ variable,
+        # and allows you to read bytes from the request body.
+        #
+        # See also: PEP 3333
+        if req.content_length in (None, 0):
+            # Nothing to do
+            return
+
+        body = req.stream.read()
+        if not body:
+            raise falcon.HTTPBadRequest('Empty request body',
+                                        'A valid JSON document is required.')
+
+        try:
+            req.context['doc'] = json.dumps(body.decode('utf-8'))
+
+        except (ValueError, UnicodeDecodeError):
+            raise falcon.HTTPError(falcon.HTTP_753,
+                                   'Malformed JSON',
+                                   'Could not decode the request body. The '
+                                   'JSON was incorrect or not encoded as '
+                                   'UTF-8.')
+
+    def process_response(self, req, resp, resource):
+        if 'result' not in req.context:
+            return
+
+class PostEvent(object):
+
+    def __init__(self, log_name):
+        self.log_name = "logs/{0}".format(log_name)
+
+    def on_post(self, req, resp):
+        payload = json.loads(req.context['doc'])
+        resp.status = falcon.HTTP_200
+        with open(self.log_name, 'a') as f:
+            f.write(payload)
